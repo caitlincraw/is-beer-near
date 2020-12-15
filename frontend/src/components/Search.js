b8f3067177e07b148f2d85c69bdc49958de26d77
Caitlin Crawford <caitlin.crawford16@gmail.com>
2020-12-14 19:56:21 -0500

    More css edits...

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -12,11 +12,14 @@ const Search = (store) => {
     }
 
     const filterRender = () => {
-        if (beerType === "All Beers") {
+        if (!store.beerStore.beers.length) {
+            return "There are no beers available at this time... Sorry!";
+        }
+        else if (beerType === "All Beers") {
             return store.beerStore.beers.map(beer => <BeerCard key={beer.id} name={beer.name} type={beer.type} info={beer.info} breweryName={beer.Brewery.name} breweryURL={beer.Brewery.website} />);
         } else {
             const filteredBeers = store.beerStore.beers.filter(beer => beer.type === beerType);
-            return filteredBeers.map(beer => <BeerCard key={beer.id} name={beer.name} type={beer.type} info={beer.info} breweryName={beer.Brewery.name} breweryURL={beer.Brewery.website} />);
+            return filteredBeers ? filteredBeers.map(beer => <BeerCard key={beer.id} name={beer.name} type={beer.type} info={beer.info} breweryName={beer.Brewery.name} breweryURL={beer.Brewery.website} />) : `There are no ${beerType} beers at the moment... Booooooooo.`;
         }
     }
 
